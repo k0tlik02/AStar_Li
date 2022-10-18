b8e98f9322322ca21f7a0f5e262ab10aae2b648f
Kotlyar Oleksandr <kotlyar7733@gmail.com>
2022-10-19 02:01:55 +0300

    first commit

---
+++ A_Star_C_Sharp/A_Star_C_Sharp/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace A_Star_c_Sharp
+{
+    class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int gCost { get; set; }
+        public int hCost { get; set; }
+        public int fCost => gCost + hCost;
+        public Node Parent { get; set; }
+
+        public void calculateG()
+        {
+            if (Math.Abs(Parent.X - X) + Math.Abs(Parent.Y - Y) == 1)
+            {
+                gCost = Parent.gCost + 10;
+            }
+            else if (Math.Abs(Parent.X - X) + Math.Abs(Parent.Y - Y) == 2)
+            {
+                gCost = Parent.gCost + 14;
+            }
+            else gCost = Parent.gCost;
+        }
+
+        public void calculateH(Node node, int x, int y)
+        {
+            this.hCost = (Math.Abs(node.X - x) + Math.Abs(node.Y - y)) * 10;
+        }
+    }
+    class Program
+    {
+        public int calculateH(Node node, int x, int y)
+        {
+            int h = (Math.Abs(node.X - x) + Math.Abs(node.Y - y)) * 10;
+            return h;
+        }
+
+        private static List<Node> GetNeighbours(int[,] field, Node currentNode, Node targetNode)
+        {
+            var possibleTiles = new List<Node>()
+            {
+                new Node { X = currentNode.X, Y = currentNode.Y - 1, Parent = currentNode },
+                new Node { X = currentNode.X, Y = currentNode.Y + 1, Parent = currentNode },
+                new Node { X = currentNode.X - 1, Y = currentNode.Y, Parent = currentNode },
+                new Node { X = currentNode.X + 1, Y = currentNode.Y, Parent = currentNode },
+                new Node { X = currentNode.X + 1, Y = currentNode.Y - 1, Parent = currentNode },
+                new Node { X = currentNode.X + 1, Y = currentNode.Y + 1, Parent = currentNode },
+                new Node { X = currentNode.X - 1, Y = currentNode.Y - 1, Parent = currentNode },
+                new Node { X = currentNode.X - 1, Y = currentNode.Y + 1, Parent = currentNode },
+            };
+
+
+            possibleTiles.ForEach(tile => tile.calculateH(targetNode, tile.X, tile.Y));
+            possibleTiles.ForEach(tile => tile.calculateG());
+            
+
+            var maxX = field.GetUpperBound(0);
+            var maxY = maxX;
+
+            return possibleTiles
+                    .Where(tile => tile.X >= 0 && tile.X <= maxX)
+                    .Where(tile => tile.Y >= 0 && tile.Y <= maxY)
+                    .Where(tile => field[tile.Y, tile.X] != 1)
+                    .ToList();
+        }
+
+   
+        static void Main(string[] args)
+        {
+            int[,] field = { { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, { 0, 0, 1, 0, 1, 1, 0, 0, 1, 0 }, { 0, 1, 0, 1, 1, 1, 0, 0, 1, 1 }, { 0, 1, 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 1, 0, 1, 0, 0, 1, 1, 0, 1 }, { 1, 0, 0, 1, 1, 1, 0, 0, 1, 0 }, { 0, 0, 0, 1, 1, 0, 0, 1, 0, 0 }, { 0, 1, 0, 0, 1, 1, 1, 0, 1, 0 }, { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0 } };
+
+            Write_field(ref field, 2, 3, 3, 6);
+            Console.WriteLine("\n\n\n\n\n");
+         //   Console.WriteLine(field[5, 2]);
+            var start = new Node();
+            start.Y = 2;
+            start.X = 3;
+
+            var finish = new Node();
+            finish.Y = 3;
+            finish.X = 6;
+
+            start.calculateH(finish, start.X, start.Y);
+            start.gCost = 0;
+            
+            var openList = new List<Node>();
+            openList.Add(start);
+            var closedList = new List<Node>();
+
+
+            while (openList.Any())
+            {
+                var checkNode = openList.OrderBy(x => x.fCost).First();
+
+                if (checkNode.X == finish.X && checkNode.Y == finish.Y)
+                {
+                    var node = checkNode;
+                   // Console.WriteLine("Retracing steps backwards...");
+                    while (true)
+                    {
+                        //Console.WriteLine($"{node.X} : {node.Y}");
+                        if (field[node.Y, node.X] == 0)
+                        {
+                            field[node.Y, node.X] = 10;
+                        }
+                        node = node.Parent;
+                        if (node == null)
+                        {
+                            Write_field(ref field, 2, 3, 3, 6);
+                            return;
+                        }
+                    }
+                }
+
+                closedList.Add(checkNode);
+                openList.Remove(checkNode);
+
+                var neighbours = GetNeighbours(field, checkNode, finish);
+
+                foreach(var neighbour in neighbours)
+                {
+                    if (closedList.Any(x => x.X == neighbour.X && x.Y == neighbour.Y))
+                        continue;
+
+                    if (openList.Any(x => x.X == neighbour.X && x.Y == neighbour.Y))
+                    {
+                        var existingNode = openList.First(x => x.X == neighbour.X && x.Y == neighbour.Y);
+
+                        if (existingNode.fCost > checkNode.fCost)
+                        {
+                            openList.Remove(existingNode);
+                            openList.Add(neighbour);
+                        }
+                    }
+                    else
+                    {
+                        openList.Add(neighbour);
+                    }
+                }
+            }
+            Console.WriteLine("No Path Found!");
+        }
+
+        static void Write_field(ref int[,] field, int a1, int a2, int b1, int b2)
+        {
+            int rows = field.GetUpperBound(0) + 1;
+            int columns = field.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (i == a1 && j == a2)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(" A");
+                        Console.ResetColor();
+                    }
+                    else if (i == b1 && j == b2)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(" B");
+                        Console.ResetColor();
+                    }
+                    else if (field[i, j] == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write(" " + field[i, j]);
+                        Console.ResetColor();
+                    }
+                    else if (field[i, j] == 1)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.Write("  ");
+                        Console.ResetColor();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PIIS_Lab_1/PIIS_Lab_1/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+
+namespace PIIS_Lab_1
+{   
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            int[,] field = { { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, { 0, 0, 1, 0, 1, 1, 0, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 0, 1, 1 }, { 0, 1, 0, 0, 0, 1, 1, 0, 0, 0 }, { 0, 1, 0, 1, 0, 0, 1, 1, 0, 1 }, { 1, 0, 0, 1, 1, 1, 0, 0, 1, 0 }, { 0, 0, 0, 1, 1, 0, 0, 1, 0, 0 }, { 0, 1, 0, 0, 1, 1, 1, 0, 1, 0 }, { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0 } };
+
+            int n = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[,] Field = new int[n, n];
+            List<(int, int)> solution = new List<(int, int)>();
+
+          //  Write_field(ref field, 2, 3, 3, 5);
+            GenerationOfTheField(ref n, ref Field);
+
+            WriteField(ref Field, 15, 5, ref solution);
+
+            Algo_Li(ref Field, ref solution, 7, 13, 15, 5);
+
+            Console.WriteLine("\n\n\n");
+
+            WriteField(ref Field, 15, 5, ref solution);
+
+          //  Console.WriteLine("\n\n\n");
+
+            
+        }
+
+        static void GenerationOfTheField(ref  int n, ref int[,] Field)
+        {
+            Random rnd = new Random();
+            
+           
+            double d_blocks = 0.17 * n * n;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Field[i,j] = 0;
+                }
+            Math.Round(d_blocks);
+            while (d_blocks>0)
+            {
+                int i = rnd.Next(0, 23);
+                int j = rnd.Next(0, 23);
+                if (Field[i, j] == 0)
+                    Field[i, j] = -1;
+                else
+                {
+                    while (Field[i, j] != 0)
+                    {
+                        i = rnd.Next(0, 23);
+                        j = rnd.Next(0, 23);
+                    }
+                    Field[i, j] = -1;
+                }
+                d_blocks--;
+            }
+        }
+
+
+        static void Algo_Li(ref int[,] Field, ref List<(int, int)> solution, int a1, int a2, int b1, int b2)
+        {
+            int i = a1;
+            int j = a2;
+            int count = 0;
+            int border = Field.GetUpperBound(0);
+           
+            if (Field[i + 1, j] <= border && Field[i + 1, j] != -1) Field[i + 1, j] = 1;
+            if (Field[i - 1, j] >= 0 && Field[i - 1, j] != -1) Field[i - 1, j] = 1;
+            if (Field[i, j + 1] <= border && Field[i, j + 1] != -1) Field[i, j + 1] = 1;
+            if (Field[i, j - 1] >= 0 && Field[i, j - 1] != -1) Field[i, j - 1] = 1;
+
+            bool b = true;
+            while (b)
+            {
+                count++;
+                for (int i1 = 1; i1 < border; i1++)
+                {
+                    for (int i2 = 1; i2 < border; i2++)
+                    {
+                        if (Field[i1, i2] == count)
+                        {
+                            if (((i1 + 1) == b1 && i2 == b2) || ((i1 - 1) == b1 && i2 == b2) || (i1 == b1 && (i2 + 1) == b2) || (i1 == b1 && (i2 - 1) == b2))
+                            {
+                                b = false;
+                                break; 
+                            }
+
+
+                            if (Field[i1 + 1, i2] == 0) Field[i1 + 1, i2] = count + 1;
+                            if (Field[i1 - 1, i2] == 0) Field[i1 - 1, i2] = count + 1;
+                            if (Field[i1, i2 + 1] == 0) Field[i1, i2 + 1] = count + 1;
+                            if (Field[i1, i2 - 1] == 0) Field[i1, i2 - 1] = count + 1;
+                        }
+                    }
+                }
+
+            }
+
+            i = b1;
+            j = b2;
+            //solution.Add((b1, b2));
+            
+            while (count>0)
+            {
+                if (Field[i + 1, j] == count) { solution.Add((i + 1, j)); i += 1; count--; continue; }
+                if (Field[i - 1, j] == count) { solution.Add((i - 1, j)); i -= 1; count--; continue; }
+                if (Field[i, j + 1] == count) { solution.Add((i, j + 1)); j += 1; count--; continue; }
+                if (Field[1, j - 1] == count) { solution.Add((i, j - 1)); j += 1; count--; continue; }
+            }
+        }
+
+        static void WriteField(ref int[,] Field, int b1, int b2, ref List<(int, int)> solution)
+        {
+            int rows = Field.GetUpperBound(0) + 1;  
+            int columns = Field.Length / rows;
+            bool b = true;
+            for (int i=0; i<rows; i++)
+            {
+                for (int j=0; j<columns; j++)
+                {
+                    b = true;
+                    for (int k = 0; k < solution.Count; k++)
+                    {
+                        if (i == solution[k].Item1 && j == solution[k].Item2)
+                        {
+                            b = false;
+                            if (Field[i, j] / 10 == 0)
+                            {
+                                Console.BackgroundColor = ConsoleColor.Yellow;
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.Write(" " + Field[i, j]);
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Yellow;
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.Write(Field[i, j]);
+                                Console.ResetColor();
+                            }
+                        }
+                    }
+                    if (b == true)
+                    {
+                        if (i == 7 && j == 13)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(" A");
+                            Console.ResetColor();
+                        }
+                        else if (i == b1 && j == b2)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(" B");
+                            Console.ResetColor();
+                        }
+                        else if (Field[i, j] == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write(" 0");
+                            Console.ResetColor();
+                        }
+                        else if (Field[i, j] < 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            Console.Write("  ");
+                            Console.ResetColor();
+                        }
+                        else if (Field[i, j] / 10 == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write(" " + Field[i, j]);
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write(Field[i, j]);
+                            Console.ResetColor();
+                        }
+                    }
+                    else continue;
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
